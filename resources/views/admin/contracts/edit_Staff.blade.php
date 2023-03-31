758340bcfb51501639693c5088b58de6e52aa190
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-03-31 16:22:32 +0500

    push editf iles

--- resources/views/admin/contracts/edit_Staff.blade.php
+++ resources/views/admin/contracts/edit_Staff.blade.php
@@ -127,8 +127,7 @@ if(!empty(Session::get('locale')))
                     <div class="form-group row {{ $errors->has('address') ? 'has-error' : '' }}">
                       <label class="col-3">{{trans('admin.address')}} </label>
                       <div class="col-9">
-                     <textarea rows="4" cols="50" class = "form-control form-control-solid" name="address" placeholder="Enter Address here...">
-                        </textarea><span class="text-danger">{{ $errors->first('address') }}</span>
+                     <textarea rows="4" cols="50" class = "form-control form-control-solid" name="address" placeholder="Enter Address here..."></textarea><span class="text-danger">{{ $errors->first('address') }}</span>
                       </div>
                     </div>
                     <div class="form-group row {{ $errors->has('contract_start_date') ? 'has-error' : '' }}">
