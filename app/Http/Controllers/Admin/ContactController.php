311eb02edc7e68f67910337d0ee9a84cc1eb4f7b
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-03-31 16:10:32 +0500

    push

--- app/Http/Controllers/Admin/ContactController.php
+++ app/Http/Controllers/Admin/ContactController.php
@@ -127,7 +127,7 @@ class ContactController extends Controller
     {
         
         $title = "Contract Types";
-        $contract_types = ContractType::latest()->get();
+        $contract_types = ContractType::where('added_by',auth()->user()->id)->latest()->get();
         $users = User::where('is_admin', '0')->where('user_type','user')->get();
 		return view('admin.contracts.create',compact('title','contract_types','users'));
     }
@@ -192,7 +192,7 @@ class ContactController extends Controller
         
         $title = "Contract Edit";
         $contract = Contract::with('user')->find($id);
-        $contract_types = ContractType::latest()->get();
+        $contract_types = ContractType::where('added_by',auth()->user()->id)->latest()->get();
         $users = User::where('is_admin', '0')->where('user_type','user')->get();
       
 		return view('admin.contracts.edit', compact('contract','title','contract_types','users'));
--- resources/views/admin/clients/index.blade.php
+++ resources/views/admin/clients/index.blade.php
@@ -81,7 +81,7 @@ if(!empty(Session::get('locale')))
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                        <h4 class="modal-title" id="myModalLabel">{{trans('admin.Company')}} {{trans('admin.detail ')}}</h4>
+                        <h4 class="modal-title" id="myModalLabel">{{trans('admin.Company')}} {{trans('admin.detail')}}</h4>
                     </div>
                     <div class="modal-body"></div>
                     <div class="modal-footer">
